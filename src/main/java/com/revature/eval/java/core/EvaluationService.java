9d087060be6ebc694413cf18430f656f25daf4e9
Bader Juma <baderwady@gmail.com>
2020-01-16 22:54:46 -0500

    Solved cleanPhoneNumber

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -202,8 +202,15 @@ public class EvaluationService {
 	 * NANP-countries, only 1 is considered a valid country code.
 	 */
 	public String cleanPhoneNumber(String string) {
-		// TODO Write an implementation for this method declaration
-		return null;
+		
+		String cleanNumber = string.replaceAll("\\D", "");
+		if(cleanNumber.length() > 11 || cleanNumber.length() < 10)
+		throw new IllegalArgumentException("Input valid phone number!");
+
+		return cleanNumber;
+	
+		
+		
 	}
 
 	/**
